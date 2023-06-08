f4d372cdd0877786c4cd5601f8a8b01ed1c08c8e
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2023-06-08 15:09:37 -0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -6,11 +6,11 @@ export default function InstructionsComponent() {
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					create<span>-web3-dapp</span>
+					DeWork4Us<span>Dapp</span>
 				</h1>
 				<p>
-					Get started by editing this page in{" "}
-					<span>/pages/index.js</span>
+					Gerenciador de Contratos{" "}
+					<span>DeWork4Us</span>
 				</p>
 			</header>
 
