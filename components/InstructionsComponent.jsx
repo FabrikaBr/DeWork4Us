4f9a4a735ef1311b5c5be23b53ae2d4c7904f0a5
FabrikaBr <contact@fabrikabr.io>
2023-07-01 18:07:25 -0300

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -48,10 +48,10 @@ export default function InstructionsComponent() {
 				</a>
 			</div>
 			<div className={styles.footer}>
-				<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+				<a href="https://fabrikabr.io/" target={"_blank"}>
 					<img
 						id="badge-button"
-						style={{ width: "240px", height: "53px" }}
+						style={{ width: "287px", height: "58px" }}
 						src="https://github.com/FabrikaBr/DeWork4Us/blob/main/public/Captura%20de%20tela%202023-07-01%20172838.png"
 						alt="DeWork4Us by Fabrikabr.io"
 					/>
